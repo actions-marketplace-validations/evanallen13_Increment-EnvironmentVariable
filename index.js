f51d4b00f5f9b48e8707f214ecfa601b32aafe6c
Evan Allen <eallen@xpirit.com>
2023-09-11 15:02:26 -0400

    hello world

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+// const core = require("@actions/core");
+// const github = require("@actions/github");
 
-const name = core.getInput("name");
+// const name = core.getInput("name");
 
-console.log(`Hello ${name}!`);
+console.log(`Hello World!`);
